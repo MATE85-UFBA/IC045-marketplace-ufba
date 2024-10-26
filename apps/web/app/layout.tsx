43854fd31ca090a10cea6b39086f53cd0b60257a
Joao Fernandes <joao.fernandes@eurekalabs.com.br>
2024-10-26 18:34:45 -0300

    feat: front-components

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/modules/components/header/header";
+import Footer from "@/modules/components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
