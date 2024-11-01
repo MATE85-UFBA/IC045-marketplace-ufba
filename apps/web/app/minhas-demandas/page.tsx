9012f6d2772b578d3d2abdafaa6f5482d7c074c8
Joao Fernandes <joao.fernandes@eurekalabs.com.br>
2024-11-01 10:45:04 -0300

    fix: padding

--- apps/web/app/minhas-demandas/page.tsx
+++ apps/web/app/minhas-demandas/page.tsx
@@ -31,7 +31,7 @@ const MinhasDemandas = () => {
   ];
   return (
     <main className="flex justify-center ">
-      <section className="flex flex-col w-full max-w-7xl px-10 pt-12 gap-6">
+      <section className="flex flex-col w-full max-w-7xl pt-12 gap-6">
         <div className="flex justify-between">
           <h1 className="font-bold text-4xl text-blue-strong">
             Minhas Demandas
