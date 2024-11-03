84c332d4b81d5fb82b43c1dfc1d2e03b8100ad19
João Pedro Fernandes <62401441+JhonPrFer@users.noreply.github.com>
2024-11-03 18:22:57 -0300

    Feat/front lista demandas (#51)
    Co-authored-by: Joao Fernandes <joao.fernandes@eurekalabs.com.br>
    Co-authored-by: Eduardo Vasconcelos <107057997+EduardoVasconceloss@users.noreply.github.com>
    Co-authored-by: root <root@srv623232.hstgr.cloud>
    Co-authored-by: Felipe J. R. Vieira <40006185+felipejrvieira@users.noreply.github.com>

--- apps/web/modules/components/header/header.tsx
+++ apps/web/modules/components/header/header.tsx
@@ -4,39 +4,41 @@ import { Button } from "@/components/ui/button";
 import "../../../app/globals.css";
 const Header = () => {
   return (
-    <header className="flex justify-evenly p-4 content-center shadow-custom bg-white">
-      <div>
-        <Link href={"/"} className="flex items-center gap-3">
-          <img src="/logo.png" alt="logo ufba" />
-          <h1 className="text-3xl font-bold text-blue-strong">Nexus</h1>
-        </Link>
-      </div>
-      <div className="flex gap-5 items-center">
-        <Link href={"/"} className="items-center font-bold text-blue-strong">
-          Encontrar Demandas
-        </Link>
-        <Link href={"/"} className="items-center font-bold text-blue-strong">
-          Encontrar Grupo de Pesquisa
-        </Link>
-      </div>
-      <div className="flex gap-2.5 items-center content-center">
-        <Button
-          variant={"outline"}
-          asChild
-          className="rounded-full py-2.5 px-8 border-primary"
-        >
-          <Link
-            href={"/"}
-            className="font-medium text-primary bg-white text-base"
+    <header className="shadow-custom bg-white flex justify-center">
+      <div className="flex justify-between py-4 content-center w-full max-w-7xl">
+        <div>
+          <Link href={"/"} className="flex items-center gap-3">
+            <img src="/logo.png" alt="logo ufba" />
+            <h1 className="text-3xl font-bold text-blue-strong">Nexus</h1>
+          </Link>
+        </div>
+        <div className="flex gap-5 items-center">
+          <Link href={"/"} className="items-center font-bold text-blue-strong">
+            Encontrar Demandas
+          </Link>
+          <Link href={"/"} className="items-center font-bold text-blue-strong">
+            Encontrar Grupo de Pesquisa
+          </Link>
+        </div>
+        <div className="flex gap-2.5 items-center content-center">
+          <Button
+            variant={"outline"}
+            asChild
+            className="rounded-full py-2.5 px-8 border-primary"
           >
-            Entrar
-          </Link>
-        </Button>
-        <Button asChild className="rounded-full py-2.5 px-8">
-          <Link href={"/"} className="font-medium">
-            Cadastar-se
-          </Link>
-        </Button>
+            <Link
+              href={"/"}
+              className="font-medium text-primary bg-white text-base"
+            >
+              Entrar
+            </Link>
+          </Button>
+          <Button asChild className="rounded-full py-2.5 px-8">
+            <Link href={"/"} className="font-medium">
+              Cadastar-se
+            </Link>
+          </Button>
+        </div>
       </div>
     </header>
   );
