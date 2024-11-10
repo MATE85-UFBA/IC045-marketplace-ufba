e42ee11c921636ed9574a2788ae300d94760cc0d
Matheus Marçal <matheuscpm27@hotmail.com>
2024-11-10 15:33:39 -0300

    build research group controller, controller specs and dto modules

--- apps/api/src/research-group/research-group.controller.ts
+++ apps/api/src/research-group/research-group.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+    Request,
+    UseGuards,
+} from '@nestjs/common';
+import { ResearchGroupsService } from './research-group.service';
+import { ChangePasswordDto, CreateResearchGroupDto } from './research-group.dto';
+import { JwtAuthGuard } from '@/auth/auth.guard';
 
-@Controller('research-groups')
-export class ResearchGroupsController {}
+@Controller('researchGroups')
+export class ResearchGroupsController {
+    constructor(private readonly researchGroupsSevice: ResearchGroupsService) { }
+
+    @Post()
+    create(@Body() researchGroup: CreateResearchGroupDto) {
+        return this.researchGroupsSevice.create(researchGroup);
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.researchGroupsSevice.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() researchGroup: CreateResearchGroupDto) {
+        return this.researchGroupsSevice.update(id, researchGroup);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.researchGroupsSevice.delete(id);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('/change-password')
+    changePassword(@Body() data: ChangePasswordDto, @Request() req: { researchGroup: { researchGroupId: string } }) {
+        const id = req.researchGroup.researchGroupId
+        return this.researchGroupsSevice.changePassword(id, data);
+    }
+}
--- apps/api/src/research-group/tests/research-group.controller.spec.ts
+++ apps/api/src/research-group/tests/research-group.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResearchGroupsController } from '../research-group.controller';
+import { ResearchGroupsService } from '../research-group.service';
+import { PrismaService } from '@/infra/database/prisma.service';
 
 describe('ResearchGroupsController', () => {
   let controller: ResearchGroupsController;
@@ -7,6 +9,7 @@ describe('ResearchGroupsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ResearchGroupsController],
+      providers: [ResearchGroupsService, PrismaService]
     }).compile();
 
     controller = module.get<ResearchGroupsController>(ResearchGroupsController);
