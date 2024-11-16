17b50f4f636383a7b9c33c81fcef398986f87cd7
Israel <israelped10@gmail.com>
2024-11-16 15:53:04 -0300

    Feat: add user creation functionality with company and researcher roles

--- apps/api/src/user/user.dto.ts
+++ apps/api/src/user/user.dto.ts
@@ -1,5 +1,22 @@
-import { IsEmail, IsEnum, IsNotEmpty, Matches } from 'class-validator';
-import { UserRole } from '@prisma/client';
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, Matches } from 'class-validator';
+import { ResearcherType, UserRole } from '@prisma/client';
+
+export class CreateCompanyDto {
+  contactName?: string;
+
+  @IsEmail()
+  contactEmail?: string;
+
+  contactPhone?: string;
+}
+
+export class CreateResearcherDto {
+  urlLattes?: string;
+
+  @IsEnum(ResearcherType)
+  researcherType?: ResearcherType;
+}
+
 export class CreateUserDto {
   @IsNotEmpty()
   name: string;
@@ -17,6 +34,14 @@ export class CreateUserDto {
 
   @IsEnum(UserRole)
   role: UserRole;
+
+  @IsOptional()
+  company?: CreateCompanyDto;
+
+  @IsOptional()
+  researcher?: CreateResearcherDto;
+
+
 }
 
 export class UpdateUserDto {
