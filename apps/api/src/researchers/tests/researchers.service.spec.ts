b1671d14fe29776608573abce8fe12eef9ee72d7
matheus.barretto <matheus.barretto2001@gmail.com>
2024-11-24 18:01:31 -0300

    feat(backend): adicionei teste de integração basico para serviço

--- apps/api/src/researchers/tests/researchers.service.spec.ts
+++ apps/api/src/researchers/tests/researchers.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResearchersService } from '../researchers.service';
+import { PrismaService } from '@/infra/database/prisma.service';
+import { url } from 'inspector';
+import { ResearcherType } from '@prisma/client';
 
 describe('ResearchersService', () => {
   let service: ResearchersService;
@@ -14,5 +17,47 @@ describe('ResearchersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+});
+
+describe('Integration test - ResearchersService - findOne', () => {
+  let service: ResearchersService;
+  let prismaService: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ResearchersService, PrismaService],
+    }).compile();
+
+    service = module.get<ResearchersService>(ResearchersService);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  it('Should return researcher data when researcher is found', async () => {
+    const mockResearcher = {
+      userId: '123e4567-e89b-12d3-a456-426614174000',
+      name: 'John Doe',
+      email: 'john.doe@email.com',
+      urlLattes: 'https://lattes.example.com/johndoe',
+      img: 'https://example.com/image.jpg',
+      researcherType: ResearcherType.STUDENT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    jest
+      .spyOn(prismaService.researcher, 'findUnique')
+      .mockResolvedValue(mockResearcher);
+
+    const result = await service.findOne(mockResearcher.userId, false);
+
+    expect(result).toEqual({
+      userId: mockResearcher.userId,
+      name: mockResearcher.name,
+      email: mockResearcher.email,
+      urlLattes: mockResearcher.urlLattes,
+      img: mockResearcher.img,
+      researcherType: mockResearcher.researcherType,
+    });
   });
 });
