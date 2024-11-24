263c250315cb7e00829e195b0bf853789c7118dd
Joel Pires <joelpires70@gmail.com>
2024-11-24 08:25:06 -0300

    refact: clean code

--- apps/web/cypress/e2e/login/spec.cy.ts
+++ apps/web/cypress/e2e/login/spec.cy.ts
@@ -1,9 +1,11 @@
+import { FRONT_END_URL } from "../config";
+
 describe('Login page', () => {
   const userEmail = 'reginaldo.rossi@email.com.br';
   const userPassword = 'mycrazysecurepassword';
 
   beforeEach(() => {
-    cy.visit('http://localhost:3001/login');
+    cy.visit(`${FRONT_END_URL}/login`);
   });
 
   it('should log in successfully', () => {
@@ -36,18 +38,18 @@ describe('Login page', () => {
   });
 
   it('should show message for empty email', () => {
-    cy.get('input[id="email"]').should('be.empty'); // Check for empty email initially
-    cy.get('input[id="password"]').type('anyPassword'); // Enter any password
+    cy.get('input[id="email"]').should('be.empty');
+    cy.get('input[id="password"]').type('anyPassword');
 
     cy.get('form').submit();
-    cy.contains('O e-mail é obrigatório').should('be.visible'); // Check for "O e-mail é obrigatório" message
+    cy.contains('O e-mail é obrigatório').should('be.visible'); 
   });
 
   it('should show message for empty password', () => {
-    cy.get('input[id="email"]').type(userEmail); // Enter email
-    cy.get('input[id="password"]').should('be.empty'); // Check for empty password initially
+    cy.get('input[id="email"]').type(userEmail)
+    cy.get('input[id="password"]').should('be.empty');
 
     cy.get('form').submit();
-    cy.contains('A senha é obrigatória').should('be.visible'); // Check for "A senha é obrigatório" message
+    cy.contains('A senha é obrigatória').should('be.visible');
   });
 });
