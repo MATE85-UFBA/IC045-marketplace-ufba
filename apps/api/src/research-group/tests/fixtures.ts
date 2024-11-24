eac406a66508f15475bc26bab2f68b5a0f338a7f
Pedro Jefferson <pedrojefferson.dev@gmail.com>
2024-11-24 20:14:10 -0300

    fix build

--- apps/api/src/research-group/tests/fixtures.ts
+++ apps/api/src/research-group/tests/fixtures.ts
@@ -4,6 +4,8 @@ export const demands: Demand[] = [
   {
     id: '123e4567-e89b-12d3-a456-426614174000',
     name: 'Demand',
+    public: true,
+    description: 'teste description',
     status: 'CREATED',
     companyId: '123e4567-e89b-12d3-a456-426614174000',
     createdAt: new Date(),
@@ -12,6 +14,8 @@ export const demands: Demand[] = [
   {
     id: '123e4567-e89b-12d3-a456-831279238193',
     name: 'Demand 2',
+    public: true,
+    description: 'teste description',
     status: 'CREATED',
     companyId: '123e4567-e89b-12d3-a456-831279238193',
     createdAt: new Date(),
@@ -20,6 +24,8 @@ export const demands: Demand[] = [
   {
     id: '123123129-e89b-12d3-a456-831279238193',
     name: 'Demand 3',
+    public: true,
+    description: 'teste description',
     status: 'CREATED',
     companyId: '123e4567-e89b-12d3-a456-831279238193',
     createdAt: new Date(),
