70670f8862718f10f16bf4c2147883812ebefded
matheus.barretto <matheus.barretto2001@gmail.com>
2024-11-26 21:08:56 -0300

    fix(backend): Atualizei testes de pesquisadores e fiz ajustes de lint

--- apps/api/src/researchers/tests/researchers.controller.spec.ts
+++ apps/api/src/researchers/tests/researchers.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResearchersController } from '../researchers.controller';
+import { PrismaService } from '@/infra/database/prisma.service';
+import { ResearchersService } from '../researchers.service';
 
 describe('ResearchersController', () => {
   let controller: ResearchersController;
@@ -7,6 +9,7 @@ describe('ResearchersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ResearchersController],
+      providers: [PrismaService, ResearchersService],
     }).compile();
 
     controller = module.get<ResearchersController>(ResearchersController);
