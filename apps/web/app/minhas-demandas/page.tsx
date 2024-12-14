0b8e74e3c3df17f439e2661f6dd62c5cd99e0ec0
Paulo Macêdo <p.ricardomacedo@hotmail.com>
2024-12-13 23:01:48 -0300

    refactor: Trocar todos os títulos de 'Demanda' para 'demanda'

--- apps/web/app/minhas-demandas/page.tsx
+++ apps/web/app/minhas-demandas/page.tsx
@@ -1,23 +1,22 @@
 "use client";
-import { Button } from '@/components/ui/button';
-import { CustomIcon } from '@/modules/components/icon/customIcon';
-import MinhasDemandasFilter from '@/modules/minhas-demandas/components/filter/minhasDemandasFilter';
-import MinhasDemandasTable from '@/modules/minhas-demandas/components/table/minhasDemandasTable';
-import { IoIosAddCircleOutline } from 'react-icons/io';
-import useGetMyDemands from '@/api/demandas/use-get-my-demands';
-import { useRouter } from 'next/navigation';
-import useDeleteDemand from '@/api/demandas/use-delete-demand';
-import { useToast } from '@/hooks/use-toast';
+import { Button } from "@/components/ui/button";
+import { CustomIcon } from "@/modules/components/icon/customIcon";
+import MinhasDemandasFilter from "@/modules/minhas-demandas/components/filter/minhasDemandasFilter";
+import MinhasDemandasTable from "@/modules/minhas-demandas/components/table/minhasDemandasTable";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import useGetMyDemands from "@/api/demandas/use-get-my-demands";
+import { useRouter } from "next/navigation";
+import useDeleteDemand from "@/api/demandas/use-delete-demand";
+import { useToast } from "@/hooks/use-toast";
 
 const MinhasDemandas = () => {
   const { data: demands = [] } = useGetMyDemands();
   const router = useRouter();
-  const { toast } = useToast()
-
+  const { toast } = useToast();
 
   const deleteDemandaMutation = useDeleteDemand(
-    () => toast({title: "Demanda removida com sucesso!"}),
-    () => toast({title: "Não foi possivel remover demanda."})
+    () => toast({ title: "Demanda removida com sucesso!" }),
+    () => toast({ title: "Não foi possivel remover demanda." })
   );
 
   const handleRedirect = () => {
@@ -41,7 +40,7 @@ const MinhasDemandas = () => {
       <section className="flex flex-col w-full max-w-7xl pt-12 gap-6">
         <div className="flex justify-between">
           <h1 className="font-bold text-2xl text-blue-strong sm:text-4xl">
-            Minhas Demandas
+            Minhas demandas
           </h1>
           <Button className="rounded-full" onClick={handleRedirect}>
             <CustomIcon icon={IoIosAddCircleOutline} className="!size-5" /> Nova
