b87a75ff3958e961c7eac06218636bed5cf28376
QuevedoMatheus <matheusquevedobio@gmail.com>
2024-12-16 10:42:53 -0300

    remoção dos imports

--- apps/web/app/encontrar-demandas/page.tsx
+++ apps/web/app/encontrar-demandas/page.tsx
@@ -2,10 +2,10 @@
 
 import { DemandList } from '@/components/DemandList';
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
+//import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+//import { Label } from '@/components/ui/label';
+//import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { FiSearch } from 'react-icons/fi';
 import useGetAllDemands from '@/api/use-get-all-demands';
 
